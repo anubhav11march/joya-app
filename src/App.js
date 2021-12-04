30bc85031ce6b7bb6c930befe720ffa74d23ca27
Anubhav Mittal <42939575+anubhav11march@users.noreply.github.com>
2021-12-04 19:09:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <img src={joy_logo} alt='logo' />
         <p className='para'>
           The company to disrupt the technology, coming in March 2022...
+          <br>
+          <br>
+          Koxfin is a software design and deployment partner that helps you to built successful tech products.<br>
+          We offer full-service UI/UX development for IOS, Android and web platforms including backend programming, project management, testing, launching and maintenance.
         </p>
       </div>
     </div>
